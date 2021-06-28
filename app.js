14242f345dcd46da8f6f4ef6867aeebaa0d13230
Abubakar Siddique <abz.siddique@gmail.com>
2021-06-28 19:09:49 +0100

    chore: Set up heroku deployment for server

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import router from './views/router.js'
+import cors from 'cors'
 
 // import logger from './middleware/logger.js'
 import errorHandler from './middleware/errorHandler.js'
@@ -11,11 +12,12 @@ app.use(express.json())
 // ? Using my own logging middleware.
 // app.use(logger)
 
+app.use(cors())
+
 app.use('/api', router)
 
 app.use('/images', express.static('/db/data'))
 
 app.use(errorHandler)
-
 
 export default app
--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,9 @@
+import dotenv from 'dotenv'
+dotenv.config()
 
 // our database URL
-export const dbURL = 'mongodb://localhost/itemdb'
-
+export const dbURI = 
+  process.env.DB_URI || 'mongodb://localhost/itemdb'
+export const port = process.env.PORT || 4000
 // Secret string for JWT
-export const secret = 'abubakarjonasdimitar'
+export const secret = process.env.SECRET || 'shhhh its a secret'
